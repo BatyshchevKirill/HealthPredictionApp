82a8c8f3bf9b3e5eeaf99c0b6a4dc1d7bc113c1c
BatyshchevKirill <k.batyshchev@innopolis.university>
2022-04-27 17:20:47 +0300

    Update AnalysisPage.java

--- src/view/pages/AnalysisPage.java
+++ src/view/pages/AnalysisPage.java
@@ -1,8 +1,15 @@
 package view.pages;
 
-public class AnalysisPage extends DefaultPage {
+import RandonForest.RandomForest;
+
+public class AnalysisPage extends HomePage {
     @Override
     public void draw() {
         System.out.println(getClass().getSimpleName());
+        System.out.println("To perform an analysis, enter your data:");
+        RandomForest forest = new RandomForest();
+        forest.getAnalysis();
+        super.goToBack();
+
     }
 }
